76fb3df7677fbbb06a6fa7f07338ad958ad86067
Daniel González García <danielgonnet@gmail.com>
2025-11-16 09:31:43 +0100

    add folder icos and auto-compelte to pwsh

--- .config/powershell/Microsoft.PowerShell_profile.ps1
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,10 @@
+if ($host.Name -eq 'ConsoleHost')
+{
+    # Install-Module -Name PSReadLine -Force
+    Import-Module PSReadLine
+}
+
+# Install-Module -Name Terminal-Icons -Repository PSGallery
+Import-Module -Name Terminal-Icons
+
 Invoke-Expression (&starship init powershell)
